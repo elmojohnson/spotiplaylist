993e5ce1db6b9991c8d876e0d5ec37b55ee58d5f
John Elmo Johnson <elmoj6@gmail.com>
2023-01-19 18:17:52 +1100

    Update: Search form pulse animation when searching tracks

--- components/forms/Search.js
+++ components/forms/Search.js
@@ -4,16 +4,20 @@ import { CgSpinner } from "react-icons/cg";
 
 const Search = ({ value, handleChange, handleSearch, isLoading }) => {
   return (
-    <div className="flex items-center space-x-2 px-3 py-2 focus-within:ring-1 focus-within:ring-primary border rounded-lg">
+    <div className={`flex items-center space-x-2 px-3 py-2 focus-within:ring-1 focus-within:ring-primary border rounded-lg ${isLoading ? "animate-pulse bg-base" : "bg-white"}`}>
       <input
         value={value}
         onChange={handleChange}
         disabled={isLoading}
         placeholder="Search Tracks"
-        className="outline-none w-full"
+        className="outline-none w-full bg-transparent"
       />
-      <button onClick={handleSearch} disabled={isLoading} className="text-primary text-2xl hover:brightness-75">
-        {isLoading ? <CgSpinner className="animate-spin" /> : <MdSearch />}
+      <button
+        onClick={handleSearch}
+        disabled={isLoading}
+        className="text-neutral-400 hover:text-primary text-2xl hover:brightness-75"
+      >
+        <MdSearch />
       </button>
     </div>
   );

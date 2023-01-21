4462d1499beb5736bf40ef93c9195a9905def554
John Elmo Johnson <elmoj6@gmail.com>
2023-01-21 11:16:00 +1100

    Added: Toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import { ToastContainer } from "react-toastify";
+
+import "@/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ToastContainer position="bottom-center" closeButton={true} hideProgressBar={true} limit={3}>
+      <Component {...pageProps} />
+    </ToastContainer>
+  );
 }
